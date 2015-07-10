dcc4b9c10a833678c4f1f25f670480f5c319fd43
Judah Lamm <Judah.Lamm@gmail.com>
2015-07-09 14:51:18 -1000

    refactor complete

--- server.js
+++ server.js
@@ -10,26 +10,8 @@ var serverTime = new Date(); //numbers format
 
 var server = net.createServer(onConnect);
 
-server.listen(PORT, function() {
-  process.stdout.write('Server Listening on ' + HOST + ':' + PORT + '\n');
-});
-
-
-function onConnect(socket){
-  socket.setEncoding('utf8');
-
-  //socket.write('HTTP/1.01 ' + STATUS_OK + ' OK' + '\n' + 'Server: ' + SERVER_NAME + '\n' + 'Date: ' + Date.now() + '\n' + 'Content-Type: ' + 'text/html; charset=utf-8' + '\n' + 'Content Length: ' + 'INSERT LENGTH HERE' + '\n' + 'Connection: ' + 'keep-alive' + '\n');
-
-
-  dataListener(socket)
-
-  // socket.end()
-
-}
-
-
 var resourceList ={
-  '/' : source.index,
+  '/' : source.home,
   '/hydrogen.html' : source.hydrogen,
   '/helium.html': source.helium,
   '/404.html': source.fourOfour,
@@ -44,168 +26,120 @@ var contentLength;
 var connection;
 var statusCode;
 
-function headerBuildWrite (data, theUri){
+var requestType;
+var theUri;
 
-  if(resourceList.hasOwnProperty(theUri){
-    statusCode = STATUS_OK;
-  }else{
-    statusCode = STATUS_NOT_FOUND;
-  }
+var header;
+var bodyMessage;
+var modifiedDateRequest;
 
-  status = 'HTTP/1.01 ' + statusCode + '\n';
-  server = 'Server: ' + SERVER_NAME + '\n';
-  date = 'Date: ' + dateTime.toUTCString() + '\n';
-  contentType = 'Content-Type: ' + 'text/html; charset=utf-8' + '\n';
-  contentLength = 'Content Length: ' + resourceList[theUri].length + '\n';
-  connection = 'Connection: ' + 'keep-alive' + '\n';
 
-  socket.write(status + server + date + contentType + contentLength + connection)
+server.listen(PORT, function() {
+  process.stdout.write('Server Listening on ' + HOST + ':' + PORT + '\n');
+});
 
+
+function onConnect(socket){
+  socket.setEncoding('utf8');
+  dataListener(socket)
 }
-
-function bodyBuildWrite (data){
-
-
-
-
-
-
-
-
-  socket.write('\n\n' + source.home);
-}
-
-function readInput(data){
-
-  var reg1 = /^\w+/g;
-  var firstWord = reg1.exec(data);
-
-  var dataArray = data.split(' ');
-  var theUri = dataArray[1];
-
-  var dateTime = new Date();
-
-
-  var reg2 = /(If-Modified-Since): (\w+, \d{1,2} \w{3} \d{4} \d{1,2}:\d{1,2}:\d{1,2} \w{3})/g;
-
-  var modifiedIf = reg2.exec(data);
-  var modDate = Date.parse(modifiedIf[2]);
-
-
-}
-
 
 function dataListener(socket){
 
   socket.on('data', function(data){
 
 
+    readInput(data);
+    requestReader(data, requestType, theUri);
 
-
-    //unsure how modified since data comes in, numbers or string number date combo
-    // need to add to if statement below the check the date
-
-    if(!modifiedIf && (dateTime-modDate) > 0){
-
-      switch(firstWord[0]) {
-        case 'GET':
-          switch (dataArr[1]){
-            case '/':
-              socket.write('HTTP/1.01 ' + STATUS_OK + ' OK' + '\n' + 'Server: ' + SERVER_NAME + '\n' + 'Date: ' + dateTime.toUTCString() + '\n' + 'Content-Type: ' + 'text/html; charset=utf-8' + '\n' + 'Content Length: ' + source.home.length + '\n' + 'Connection: ' + 'keep-alive' + '\n');
-
-            break;
-
-            case '/hydrogen.html':
-              socket.write('HTTP/1.01 ' + STATUS_OK + ' OK' + '\n' + 'Server: ' + SERVER_NAME + '\n' + 'Date: ' + dateTime.toUTCString() + '\n' + 'Content-Type: ' + 'text/html; charset=utf-8' + '\n' + 'Content Length: ' + source.hydrogen.length + '\n' + 'Connection: ' + 'keep-alive');
-              socket.write('\n\n' + source.hydrogen);
-            break;
-
-            case '/helium.html':
-              socket.write('HTTP/1.01 ' + STATUS_OK + ' OK' + '\n' + 'Server: ' + SERVER_NAME + '\n' + 'Date: ' + dateTime.toUTCString() + '\n' + 'Content-Type: ' + 'text/html; charset=utf-8' + '\n' + 'Content Length: ' + source.helium.length + '\n' + 'Connection: ' + 'keep-alive' + '\n');
-              socket.write('\n\n' + source.helium);
-            break;
-
-            case '/404.html':
-              socket.write('HTTP/1.01 ' + STATUS_NOT_FOUND + ' OK' + '\n' + 'Server: ' + SERVER_NAME + '\n' + 'Date: ' + dateTime.toUTCString() + '\n' + 'Content-Type: ' + 'text/html; charset=utf-8' + '\n' + 'Content Length: ' + source.fourOfour.length + '\n' + 'Connection: ' + 'keep-alive' + '\n');
-              socket.write('\n\n' + source.fourOfour);
-            break;
-
-            case '/css/styles.css':
-              socket.write('HTTP/1.01 ' + STATUS_OK + ' OK' + '\n' + 'Server: ' + SERVER_NAME + '\n' + 'Date: ' + dateTime.toUTCString() + '\n' + 'Content-Type: ' + 'text/html; charset=utf-8' + '\n' + 'Content Length: ' + source.styles.length + '\n' + 'Connection: ' + 'keep-alive' + '\n');
-              socket.write('\n\n' + source.styles);
-            break;
-          }
-        break;
-
-        case 'HEAD':
-          switch (dataArr[1]){
-            case '/':
-              socket.write('HTTP/1.01 ' + STATUS_OK + ' OK' + '\n' + 'Server: ' + SERVER_NAME + '\n' + 'Date: ' + dateTime.toUTCString() + '\n' + 'Content-Type: ' + 'text/html; charset=utf-8' + '\n' + 'Content Length: ' + source.home.length + '\n' + 'Connection: ' + 'keep-alive' + '\n');
-            break;
-
-            case '/hydrogen.html':
-              socket.write('HTTP/1.01 ' + STATUS_OK + ' OK' + '\n' + 'Server: ' + SERVER_NAME + '\n' + 'Date: ' + dateTime.toUTCString() + '\n' + 'Content-Type: ' + 'text/html; charset=utf-8' + '\n' + 'Content Length: ' + source.hydrogen.length + '\n' + 'Connection: ' + 'keep-alive');
-            break;
-
-            case '/helium.html':
-              socket.write('HTTP/1.01 ' + STATUS_OK + ' OK' + '\n' + 'Server: ' + SERVER_NAME + '\n' + 'Date: ' + dateTime.toUTCString() + '\n' + 'Content-Type: ' + 'text/html; charset=utf-8' + '\n' + 'Content Length: ' + source.helium.length + '\n' + 'Connection: ' + 'keep-alive' + '\n');
-            break;
-
-            case '/404.html':
-              socket.write('HTTP/1.01 ' + STATUS_NOT_FOUND + ' OK' + '\n' + 'Server: ' + SERVER_NAME + '\n' + 'Date: ' + dateTime.toUTCString() + '\n' + 'Content-Type: ' + 'text/html; charset=utf-8' + '\n' + 'Content Length: ' + source.fourOfour.length + '\n' + 'Connection: ' + 'keep-alive' + '\n');
-            break;
-
-            case '/css/styles.css':
-              socket.write('HTTP/1.01 ' + STATUS_OK + ' OK' + '\n' + 'Server: ' + SERVER_NAME + '\n' + 'Date: ' + dateTime.toUTCString() + '\n' + 'Content-Type: ' + 'text/html; charset=utf-8' + '\n' + 'Content Length: ' + source.styles.length + '\n' + 'Connection: ' + 'keep-alive' + '\n');
-            break;
-          }
-        break;
-
-        case 'POST':
-          //'I want to send you data';
-        break;
-
-        case 'PUT':
-          //'I want to change a resource';
-        break;
-
-        case 'DELETE':
-          //'I want to delete a resource';
-        break;
-
-        case 'OPTIONS':
-          //'What methods can I use?'
-        break;
-        default:
-          //'Default message'
-        break;
-      }
-    }else{
-      //no change to info, they only get body
-      switch (dataArr[1]){
-        case '/':
-          socket.write('HTTP/1.01 ' + STATUS_NO_CHANGE + ' OK' + '\n' + 'Server: ' + SERVER_NAME + '\n' + 'Date: ' + dateTime.toUTCString()  + '\n' + 'Content-Type: ' + 'text/html; charset=utf-8' + '\n' + 'Content Length: ' + source.home.length + '\n' + 'Connection: ' + 'keep-alive' + '\n');
-        break;
-
-        case '/hydrogen.html':
-          socket.write('HTTP/1.01 ' + STATUS_NO_CHANGE + ' OK' + '\n' + 'Server: ' + SERVER_NAME + '\n' + 'Date: ' + dateTime.toUTCString() + '\n' + 'Content-Type: ' + 'text/html; charset=utf-8' + '\n' + 'Content Length: ' + source.hydrogen.length + '\n' + 'Connection: ' + 'keep-alive');
-        break;
-
-        case '/helium.html':
-          socket.write('HTTP/1.01 ' + STATUS_NO_CHANGE + ' OK' + '\n' + 'Server: ' + SERVER_NAME + '\n' + 'Date: ' + dateTime.toUTCString() + '\n' + 'Content-Type: ' + 'text/html; charset=utf-8' + '\n' + 'Content Length: ' + source.helium.length + '\n' + 'Connection: ' + 'keep-alive' + '\n');
-        break;
-
-        case '/404.html':
-          socket.write('HTTP/1.01 ' + STATUS_NOT_FOUND + ' OK' + '\n' + 'Server: ' + SERVER_NAME + '\n' + 'Date: ' + dateTime.toUTCString() + '\n' + 'Content-Type: ' + 'text/html; charset=utf-8' + '\n' + 'Content Length: ' + source.fourOfour.length + '\n' + 'Connection: ' + 'keep-alive' + '\n');
-        break;
-
-        case '/css/styles.css':
-          socket.write('HTTP/1.01 ' + STATUS_NO_CHANGE + ' OK' + '\n' + 'Server: ' + SERVER_NAME + '\n' + 'Date: ' + dateTime.toUTCString() + '\n' + 'Content-Type: ' + 'text/html; charset=utf-8' + '\n' + 'Content Length: ' + source.styles.length + '\n' + 'Connection: ' + 'keep-alive' + '\n');
-        break;
-      }
-
+    if(header){
+      socket.write(header);
+    }
+    if(bodyMessage){
+      socket.write(bodyMessage);
     }
 
     socket.end();
   });
+}
+
+
+function readInput(data){
+
+  var reg1 = /^\w+/g;
+  var firstWord = reg1.exec(data);
+  requestType = firstWord[0];
+
+  var dataArray = data.split(' ');
+  theUri = dataArray[1];
+
+  var reg2 = /(If-Modified-Since): (\w+, \d{1,2} \w{3} \d{4} \d{1,2}:\d{1,2}:\d{1,2} \w{3})/g;
+  var modifiedIf = reg2.exec(data);
+
+  if(modifiedIf){
+    modifiedDateRequest = Date.parse(modifiedIf[2]);
+  }
+
+}
+
+function requestReader(data, requestType, theUri){
+
+  switch (requestType){
+
+    case 'GET':
+      headerBuildWrite(data, theUri);
+      bodyBuildWrite(data, theUri);
+    break;
+
+    case 'HEAD':
+      headerBuildWrite(data, theUri);
+    break;
+
+    case 'POST':
+      //'I want to send you data';
+    break;
+
+    case 'PUT':
+      //'I want to change a resource';
+    break;
+
+    case 'DELETE':
+      //'I want to delete a resource';
+    break;
+
+    case 'OPTIONS':
+      //'What methods can I use?'
+    break;
+    default:
+      //'Default message'
+    break;
+  }
+
+}
+
+function headerBuildWrite (data, theUri){
+  var dateTime = new Date();
+  var resourceLength;
+
+  if(resourceList.hasOwnProperty(theUri)){
+    statusCode = STATUS_OK;
+    resourceLength = resourceList[theUri].length
+  }else{
+    statusCode = STATUS_NOT_FOUND;
+    resourceLength = 0;
+  }
+
+  status = 'HTTP/1.01 ' + statusCode + '\n';
+  server = 'Server: ' + SERVER_NAME + '\n';
+  date = 'Date: ' + dateTime.toUTCString() + '\n';
+  contentType = 'Content-Type: ' + 'text/html; charset=utf-8' + '\n';
+  contentLength = 'Content Length: ' + resourceLength + '\n';
+  connection = 'Connection: ' + 'keep-alive';
+
+  header = status + server + date + contentType + contentLength + connection;
+
+}
+
+function bodyBuildWrite (data, theUri){
+  bodyMessage = '\n\n' + resourceList[theUri];
 }
